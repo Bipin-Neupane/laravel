a11bcb3b47b001b98e786399cd491df047f6bf88
Bipin-Neupane <bipinneupane909@gmail.com>
2019-05-07 14:43:34 +0545

    auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -59,6 +59,12 @@
             <li class="nav-item">
               <a class="nav-link" data-toggle="modal" data-target="#modal-info">Info</a>
             </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{route('login')}}">Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{route('register')}}">Register</a>
+            </li>
           </ul>
         </div>
       </div>
